e3359d3e446a86658469690b3ede047f8bf4732a
alexsalce <101913154+alexsalce@users.noreply.github.com>
2024-03-31 09:39:40 -0700

    updated SARAH alg

--- SARAH.py
+++ SARAH.py
@@ -7,14 +7,13 @@ Created on Sat Mar 23 12:49:12 2024
 
 import numpy as np
 import numpy.linalg as la
-from abc import ABC
 
 
-class gdData(ABC):
-    def __init__(self, name, A, b, gd_iter, batch_size,sarah_iter,epsilon=None):
-        self.name = name
-        self.A = np.array(A)
-        self.b = np.array(b)
+class gdDataSARAH():
+    def __init__(self, description, A, b, gd_iter, batch_size,sarah_iter,epsilon=None):
+        self.description = description
+        self.A = A
+        self.b = b
         self.outer_iter = gd_iter        
         self.batch_size = batch_size
         self.epsilon = epsilon
@@ -23,9 +22,9 @@ class gdData(ABC):
         self.Ab = np.c_[A.reshape(self.n, -1), b.reshape(self.n, 1)]
         self.sarah_iter = sarah_iter
         
-    def ls(cls,x, A=None, b=None):
-        A = cls.A if A is None else A
-        b = cls.b if b is None else b
+    def ls(self,x, A=None, b=None):
+        A = self.A if A is None else A
+        b = self.b if b is None else b
         L = 2 * la.norm(A)**2
         eta = 1/L
         err = A@x-b
@@ -34,36 +33,35 @@ class gdData(ABC):
         else:
             grad = 2 * A.T * err
         return grad, eta
-    def SumGradLS(cls,x,A=None,b=None):
-        A = cls.A if A is None else A
-        b = cls.b if b is None else b
-        grad = np.zeros(cls.nfeat)
+    def SumGradLS(self,x,A=None,b=None):
+        A = self.A if A is None else A
+        b = self.b if b is None else b
         err = A@x-b
-        for i in range(cls.nfeat):
-            ls = np.multiply(A[:,i].T,err)
-            grad[i] = sum(ls)/cls.n            
+        ls = A.T@err
+        grad = 2 * ls/self.n           
         return grad
-    
-# test = gdData("Cool", np.array([[5, 15], [25, 35], [45, 55],[6, 16], [26, 36], [46, 56],]), np.array([5, 20, 14, 32, 22, 38]), 100, 3, 1)
 
+def objective_function(x):
+    return np.linalg.norm(S@x - t)**2
+
+    
 # Define Parameters
 epsilon = 1e-2
 seed = 123
 outer_iter = 10
-# sarah_iter = 60
 batchsize = 1
 
 np.random.seed(seed)
 
 d = 1
 n = 100
-noise = np.random.rand(d)
+noise = np.random.rand(d,1)
 s = np.random.rand(n,d) * 10 - 5          
-t = s @ noise + np.random.randn(n)       #response (b)
+t = s @ noise + np.random.randn(n,1)       #response (b)
 S = np.column_stack((s, np.ones(n)))    #data (A)
-x0 = np.zeros(d+1)
+x0 = np.zeros((d+1,1))
 
-dataSet1 = gdData("Test data 1", S, t, 1000, 1, 500, epsilon)
+dataSet1 = gdDataSARAH("Test data 1", S, t, 150, 1, 100, epsilon)
 
 myData = dataSet1
 
@@ -79,25 +77,32 @@ counter = 0
 for _ in range (myData.outer_iter):
     vold = myData.SumGradLS(wold)
     w = wold.copy() - eta * vold.copy()
-    for j in range(0,myData.sarah_iter):
+    
+
+    for _ in range(0,myData.sarah_iter):
         i = np.random.randint(low = 0, high = (myData.n-1)) #select random index from number of observations
-        vnew = myData.ls(w,myData.A[i],myData.b[i])[0] - myData.ls(wold,myData.A[i],myData.b[i])[0] + vold
+        randA = np.expand_dims(myData.A[i], axis=0)
+        randb = np.expand_dims(myData.b[i], axis=0)
+        fi_wt = myData.SumGradLS(w,randA,randb)
+        fi_wt_old = myData.SumGradLS(wold,randA,randb)
+        vnew = fi_wt - fi_wt_old + vold
         w += -eta*np.copy(vnew)
         vold = np.copy(vnew)
-    counter+=1
-    w_list.append(w)
+        w_list.append(w)
     i = np.random.randint(0,len(w_list))
     wold = np.copy(w_list[i])
     w_list = []
 
+# checks
+objective_function(w)
+la.norm(myData.ls(w)[0])
+
 
 # standard gd loop to verify functionality
-np.random.seed(123)
-w = x0.copy()
-while(la.norm(myData.ls(w)[0])>myData.epsilon):
-    w += -myData.ls(w)[1] * myData.ls(w)[0]
-    
-np.random.seed(123)
-w = x0.copy()
-while(la.norm(myData.ls(w)[0])>myData.epsilon):
-    w += -myData.ls(w)[1] * myData.SumGradLS(w)
+# np.random.seed(123)
+# w = x0.copy()
+# while(la.norm(myData.ls(w)[0])>myData.epsilon):
+#     w += -myData.ls(w)[1] * myData.ls(w)[0]
+# objective_function(w)
+  
+  
