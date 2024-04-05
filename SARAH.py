cb10783d317f0d3d1f617e2d16223dcc69620248
alexsalce <101913154+alexsalce@users.noreply.github.com>
2024-04-04 21:50:55 -0700

    updated SARAH, seems to work now

--- SARAH.py
+++ SARAH.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Mar 23 12:49:12 2024
+Created on Thu Apr  4 18:29:36 2024
 
 @author: Alex Salce
 """
@@ -11,116 +11,97 @@ import matplotlib.pyplot as plt
 
 
 class gdDataSARAH():
-    def __init__(self, description, A, b, gd_iter, batch_size,sarah_iter,epsilon=None):
-        self.description = description
+    def __init__(self, A, b, outer_loop_iter, inner_loop_iter, batch_size, epsilon=None):
         self.A = A
         self.b = b
-        self.outer_iter = gd_iter        
-        self.batch_size = batch_size
+        self.outer_iter = outer_loop_iter  
+        self.inner_iter = inner_loop_iter
+        self.batch = batch_size
         self.epsilon = epsilon
         self.n = self.A.shape[0]
         self.nfeat = self.A.shape[1]
         self.Ab = np.c_[A.reshape(self.n, -1), b.reshape(self.n, 1)]
-        self.sarah_iter = sarah_iter
-    def Lip(self, A=None, b=None):
+    def LipLS(self, A=None, b=None):
         A = self.A if A is None else A
         b = self.b if b is None else b
         L = 2 * la.norm(A)**2
         eta = 1/L
-        return eta    
-    def GradLS(self,x, A=None, b=None):
+        return eta
+    def objective_function_ls(self,x,A=None,b=None):
         A = self.A if A is None else A
         b = self.b if b is None else b
-        err = A@x-b
-        ls = A.T@err
-        grad = 2 * ls/self.n   #converges
-        # grad = 2 * ls          #does not converge
-        return grad
-    def AvgGradLS(self,x,A=None,b=None):
+        return np.linalg.norm(A@x - b)**2
+    def sGradLS(self, x, indices, A=None, b=None,batch_size=None):
         A = self.A if A is None else A
         b = self.b if b is None else b
-        err = A@x-b
-        ls = A.T@err
-        grad = 2 * ls/self.n           
+        x = x
+        batch = self.batch if batch_size is None else batch_size
+        i = indices
+        Abatch = A[i, :]
+        bbatch = b[i]
+        grad = self.GradLS(x,Abatch,bbatch)
         return grad
-    def sGradLS(self,x, A=None, b=None):
+    def GradLS(self, x, A=None, b=None):
         A = self.A if A is None else A
         b = self.b if b is None else b
-        i = np.random.randint(low = 0, high = (self.n-1)) #select random index from number of observations
-        randA = np.expand_dims(self.A[i], axis=0)
-        randb = np.expand_dims(self.b[i], axis=0)
-        grad = self.GradLS(x,randA,randb)
+        grad = (2 * self.n / len(b)) *	A.T @ (A  @ x - b)
         return grad
 
-def objective_function(x):
-    return np.linalg.norm(S@x - t)**2
-
-    
-outer_iter = 100
-inner_iter = 200
-
-# Define Parameters
-epsilon = 1e-2
-seed = 123
-batchsize = 1
-
-np.random.seed(seed)
-
-d = 5
-n = 100
-noise = np.random.rand(d,1)
-s = np.random.rand(n,d) * 10 - 5          
-t = s @ noise + np.random.randn(n,1)       #response (b)
-S = np.column_stack((s, np.ones(n)))    #data (A)
-x0 = np.zeros((d+1,1))
-# t = s @ noise       #response (b)  NO NOISE BIAS
-# S = s    #data (A) NO NOISE BIAS
-# x0 = np.zeros((d,1))  #NO NOISE BIAS
-
-dataSet1 = gdDataSARAH("Test data 1", S, t, outer_iter, 1, inner_iter, epsilon)
-
-myData = dataSet1
 
 
+# Fixing seed to generate the same random number every time
+np.random.seed(123)
 
-#SARAH algorithm
+n = 1000  # Set the size of the matrix
+m = 50  # Set the size of the matrix
+x = np.zeros((m, 1))  # Initial point
+A = np.random.randn(n, m)  # Defining matrix A
+A = A / np.linalg.norm(A)
+b = A@(np.random.randn(m, 1)) + np.random.randn(n, 1)  # Defining vector b
+outer_loop_iter = 100
+epochs = 1
+inner_loop_iter = 500 # Number of iterations
+L = 2 * np.linalg.norm(A) ** 2
+vec = []
+batch_size = 1
+eta = 0.1/L
 
-#Least Squares Loss
-wold = np.copy(x0)
-eta = myData.Lip()
+myData = gdDataSARAH(A, b, outer_loop_iter, inner_loop_iter, batch_size)
+
+
+#initialize SARAH algorith parameters
+wold = np.copy(x)
 w_list = []
-obj = []
-counter = 0
-for _ in range (myData.outer_iter):
-    vold = myData.AvgGradLS(wold)
-    w = wold.copy() - eta * vold.copy()
-    obj.append(objective_function(w))
-    for _ in range(0,myData.sarah_iter):
-        fi_wt = myData.sGradLS(w,myData.A,myData.b)
-        fi_wt_old = myData.sGradLS(wold,myData.A,myData.b)
-        vnew = fi_wt - fi_wt_old + vold
-        w += -eta*np.copy(vnew)
-        vold = np.copy(vnew)
-        w_list.append(w)
-        obj.append(objective_function(w))
-    i = np.random.randint(0,len(w_list))
-    wold = np.copy(w_list[i])
-    w_list = []
+w_plot = []
 
-# checks
-objective_function(w)
-la.norm(myData.GradLS(w))
-
-
-plt.plot(np.log(obj))
+for _ in range(myData.outer_iter):
+    
+    w = wold
+    vold = myData.GradLS(w)
+    w = wold - eta * vold
+    
+    for i in range( 0, (myData.inner_iter-1) ):
+        i = np.random.permutation(myData.n)[:myData.batch]
+        vnew = myData.sGradLS(w, i) - myData.sGradLS(wold, i) + vold
+        wold = np.copy(w)
+        w_list.append(wold)
+        w = w - eta * vnew
+        w_plot.append(myData.objective_function_ls(w))
+    
+    wold = w_list[ np.random.permutation(range(0,len(w_list)))[0] ]
+    
+plt.plot(np.log(w_plot))
 plt.show()
 
-# standard gd loop to verify functionality
-# np.random.seed(123)
-# w = x0.copy()
-# while(la.norm(myData.GradLS(w))>myData.epsilon):
-#     w += -myData.Lip(myData.A, myData.b) * myData.GradLS(w)
-# objective_function(w)
-# la.norm(myData.GradLS(w))
-  
-  
+np.linalg.norm(myData.GradLS(w))
+
+
+
+# gd_list = []
+# wgd = np.copy(x)
+# while( la.norm(myData.GradLS(wgd)) > 1e-2 ):
+#     wgd += -1/L * myData.GradLS( wgd )
+#     gd_list.append( myData.objective_function_ls(wgd) )
+# la.norm(myData.GradLS(wgd))
+# plt.plot(np.log(gd_list))
+# plt.show()
